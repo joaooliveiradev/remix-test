022f4857ed9992a9648902192170aa8c9c1f6172
João Oliveira <joaooliveira1775@gmail.com>
2024-09-24 16:42:00 -0300

    *

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -21,12 +21,49 @@ export const loader = async () => {
 export default function Index() {
   const data = useLoaderData();
 
-  console.log({ data });
   return (
     <div className="w-full h-dvh justify-center">
       <div className="w-[968px] mx-auto">
-        dads
-        <Tabs.Root className="TabsRoot" defaultValue="tab1"></Tabs.Root>
+        <Tabs.Root className="TabsRoot" defaultValue="inbox">
+          <Tabs.List className="flex gap-6 text-[#363644] text-sm">
+            <Tabs.Trigger
+              value="inbox"
+              className="flex gap-2 items-center py-[10px] [&[data-state='active']]:border-b-2 [&[data-state='active']]:border-[#5266eb]"
+            >
+              Bill Inbox
+              <div className="px-2 py-0.5 bg-[#7073931a] text-xs rounded-[4px]">
+                3
+              </div>
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="approval"
+              className="flex gap-2 items-center py-[10px] [&[data-state='active']]:border-b-2 [&[data-state='active']]:border-[#5266eb]"
+            >
+              Bill Inbox
+              <div className="px-2 py-0.5 bg-[#7073931a] text-xs rounded-[4px]">
+                3
+              </div>
+            </Tabs.Trigger>
+            <Tabs.Trigger
+              value="scheduled"
+              className="flex gap-2 items-center py-[10px] [&[data-state='active']]:border-b-2 [&[data-state='active']]:border-[#5266eb]"
+            >
+              Bill Inbox
+              <div className="px-2 py-0.5 bg-[#7073931a] text-xs rounded-[4px]">
+                3
+              </div>
+            </Tabs.Trigger>{" "}
+            <Tabs.Trigger
+              value="paid"
+              className="flex gap-2 items-center py-[10px] [&[data-state='active']]:border-b-2 [&[data-state='active']]:border-[#5266eb]"
+            >
+              Bill Inbox
+              <div className="px-2 py-0.5 bg-[#7073931a] text-xs rounded-[4px]">
+                3
+              </div>
+            </Tabs.Trigger>
+          </Tabs.List>
+        </Tabs.Root>
       </div>
     </div>
   );
